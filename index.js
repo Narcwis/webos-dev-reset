99dbe2130153819b5b08bc456e42faa5f083e250
Narcis Cristea <narcis.cristea@massimaenergia.it>
2023-06-08 16:30:02 +0200

    wip first

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+(async () => {
+  const axios = require("axios");
+  const checkTimeLeftLink = "https://developer.lge.com/secure/CheckDevModeSession.dev?sessionToken=";
 
+  const resetTimeLink = "https://developer.lge.com/secure/ResetDevModeSession.dev?sessionToken=";
+
+  const checkResult = await axios.get(checkTimeLeftLink + process.env.LG_TOKEN);
+  console.log(checkResult.data);
+
+  const resetResult = await axios.get(resetTimeLink + process.env.LG_TOKEN);
+  console.log(resetResult.data);
+
+  const newCheckResult = await axios.get(checkTimeLeftLink + process.env.LG_TOKEN);
+  console.log(newCheckResult.data);
+})();
