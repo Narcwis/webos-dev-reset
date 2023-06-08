e6683223cbbba379aa05c96159c12364a1ab6870
Narcis Cristea <narcis.cristea@gmail.com>
2023-06-08 16:43:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 (async () => {
   const axios = require("axios");
+  const token = process.env.LG_TOKEN;
   const checkTimeLeftLink = "https://developer.lge.com/secure/CheckDevModeSession.dev?sessionToken=";
 
   const resetTimeLink = "https://developer.lge.com/secure/ResetDevModeSession.dev?sessionToken=";
 
-  const checkResult = await axios.get(checkTimeLeftLink + process.env.LG_TOKEN);
+  const checkResult = await axios.get(checkTimeLeftLink + token);
   console.log(checkResult.data);
 
-  const resetResult = await axios.get(resetTimeLink + process.env.LG_TOKEN);
+  const resetResult = await axios.get(resetTimeLink + token);
   console.log(resetResult.data);
 
-  const newCheckResult = await axios.get(checkTimeLeftLink + process.env.LG_TOKEN);
+  const newCheckResult = await axios.get(checkTimeLeftLink + token);
   console.log(newCheckResult.data);
 })();
